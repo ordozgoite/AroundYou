8442c421cccec4006c222a9b1ff55240556dce9d
Victor Ordozgoite <victorordozgoite@MacBook-Air-de-Victor.local>
2025-01-25 10:07:20 -0400

    Add LockedChatView

---
+++ WhatsGoingNearby/Component/Chat/LockedChatView.swift
@@ -0,0 +1,43 @@
+//
+//  LockedChatView.swift
+//  WhatsGoingNearby
+//
+//  Created by Victor Ordozgoite on 25/01/25.
+//
+
+import SwiftUI
+
+struct LockedChatView: View {
+    
+    let username: String
+    
+    var body: some View {
+        HStack(alignment: .bottom, spacing: 16) {
+            Image(systemName: "ellipsis.bubble")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 48, height: 48, alignment: .center)
+                .foregroundStyle(
+                    LinearGradient(
+                        colors: [Color.purple, Color.blue],
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    )
+                )
+            
+            VStack(alignment: .leading) {
+                Text("Invite \(username) to chat")
+                    .fontWeight(.bold)
+                    .font(.callout)
+                
+                Text("You can send only one message with this invite. The chat will unlock once they respond.")
+                    .foregroundStyle(.gray)
+                    .font(.callout)
+            }
+        }
+    }
+}
+
+#Preview {
+    LockedChatView(username: "amanda")
+}
