edc8d31e12a20803dc3bfcaae832f8020a41ca1e
Victor Ordozgoite <victorordozgoite@MacBook-Air-de-Victor.local>
2025-03-02 15:27:05 -0400

    Add PublishBusinessView

--- WhatsGoingNearby/Model/FormattedBusinessShowcase.swift
+++ WhatsGoingNearby/Model/FormattedBusinessShowcase.swift
@@ -7,11 +7,48 @@
 
 import Foundation
 
-enum BusinessCategory: Codable {
-    case food
-    case service
-    // TODO: completar com demais categorias
+//Eat & Drink
+//Party & Event
+//Wellness
+//Health
+//Services
+//Selling
+//Tech & Games
+//Pets
+//Education
+//Adult
+//Home
+
+enum BusinessCategory: String, Codable, CaseIterable {
+    case eatAndDrink
+    case partyAndEvent
+    case wellness
+    case health
+    case services
+    case selling
+    case techAndGames
+    case pets
+    case education
+    case adult
+    case home
+    
+    var title: String {
+        return switch self {
+        case .eatAndDrink: "Eat & Drink"
+        case .partyAndEvent: "Party & Event"
+        case .wellness: "Wellness"
+        case .health: "Health"
+        case .services:  "Services"
+        case .selling: "Selling"
+        case .techAndGames: "Tech & Games"
+        case .pets: "Pets"
+        case .education: "Education"
+        case .adult: "Adult"
+        case .home: "Home"
+        }
+    }
 }
+
 
 struct FormattedBusinessShowcase: Codable, Identifiable {
     let id: String
@@ -19,7 +56,7 @@ struct FormattedBusinessShowcase: Codable, Identifiable {
     let imageUrl: String?
     let title: String
     let description: String
-//    let category: BusinessCategory
+    let category: BusinessCategory
     let latitude: Double
     let longitude: Double
     let isLocationVisible: Bool
